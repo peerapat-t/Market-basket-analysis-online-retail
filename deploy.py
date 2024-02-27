1837534daba06b63ba15523387c805d4d9c1b887
peerapat-t <129715744+peerapat-t@users.noreply.github.com>
2024-02-27 18:39:45 +0700

    edit header

--- deploy.py
+++ deploy.py
@@ -64,7 +64,7 @@ def recommendation_prediction(buy_item_list, final_result_df):
 
 # %%
 def main():
-    st.title('Online retail selling signal')
+    st.title('Online retail corss selling system')
 
     item_option1 = st.selectbox("Item number 1:", item_list, key='item_option1')
     item_option2 = st.selectbox("Item number 2:", item_list, key='item_option2')
